417baf9ccf4f5ff2535a32cf90b672b4a5fca30a
arif-khan-dh <arif.khan.dh@gmail>
2020-10-06 15:07:29 +1100

    Vertically Centered n Social Media Fixed

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,11 @@ const btnFloating = document.querySelector('#btn-floating');
 const socialMedia = document.querySelector('#social-media');
 
 btnFloating.addEventListener('click', (event) => {
-    // socialMedia.classList.toggle('media__hide');    
-    socialMedia.classList.remove('media__hide');    
     btnFloating.classList.toggle('clicked');
     if (socialMedia.classList.contains('fade-in')) {
         socialMedia.classList.remove('fade-in');
         socialMedia.classList.add('fade-out');
+
     } else if (socialMedia.classList.contains('fade-out')) {
         socialMedia.classList.remove('fade-out');
         socialMedia.classList.add('fade-in');
