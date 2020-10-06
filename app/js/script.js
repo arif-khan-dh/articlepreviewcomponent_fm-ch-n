4a05996271091eb485dc339f66e01035ec8f18ba
arif-khan-dh <arif.khan.dh@gmail>
2020-10-06 14:34:01 +1100

    Completed

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+const btnFloating = document.querySelector('#btn-floating');
+const socialMedia = document.querySelector('#social-media');
+
+btnFloating.addEventListener('click', (event) => {
+    // socialMedia.classList.toggle('media__hide');    
+    socialMedia.classList.remove('media__hide');    
+    btnFloating.classList.toggle('clicked');
+    if (socialMedia.classList.contains('fade-in')) {
+        socialMedia.classList.remove('fade-in');
+        socialMedia.classList.add('fade-out');
+    } else if (socialMedia.classList.contains('fade-out')) {
+        socialMedia.classList.remove('fade-out');
+        socialMedia.classList.add('fade-in');
+    } else {
+        socialMedia.classList.add('fade-in');
+    }
+});
